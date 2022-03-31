ac0594fa707ea98c2923fced55937a3849740c1b
Pranav <pranavmasekar4@gmail.com>
2022-03-31 18:46:27 +0530

    bfix:Api call fixed

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,6 +1,7 @@
 // import 'dart:io';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/constants/strings.dart';
 import 'package:rest_api/model/newsinfo.dart';
@@ -10,21 +11,21 @@ class API_Manager {
     // var client = HttpClient();
     var url = Uri.parse(Strings.news_url);
     var newsModel;
-    try {
-      var response = await http.get(url); //to get the news url
-      print(response.body);
-      print(response.statusCode);
-      if (response.statusCode == 200) //for success status
-      {
-        var jsonString = response.body;
-        var jsonMap = json.decode(jsonString); //imported libraray dart: convert
-        newsModel = NewsModel.fromJson(jsonMap);
-        return newsModel;
-      }
-    } catch (e) {
-      print("Error has occured");
+    // try {
+    var response = await http.get(url); //to get the news url
+    debugPrint(response.body);
+    debugPrint(response.statusCode.toString());
+    if (response.statusCode == 200) //for success status
+    {
+      var jsonString = response.body;
+      var jsonMap = json.decode(jsonString); //imported libraray dart: convert
+      newsModel = NewsModel.fromJson(jsonMap);
       return newsModel;
     }
+    // } catch (e) {
+    //   debugPrint("Error has occured");
+    //   return newsModel;
+    // }
     return newsModel;
   }
 }
